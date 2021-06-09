c546a53f2630933d400300c45cdbac0f60aa75e3
Melissa Brewer <melramirez727@gmail.com>
2021-06-09 00:25:11 -0500

    read me

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # weather-dashboard
+## Description
+
+Weather-Dashboard is an application to find the weather for a searched city. You will be given the searched city's current and 5-Days forecast at the same time. 
+
+-City
+
+-Temperature
+
+-Humidity
+
+-Wind Speed
+
+-UV ind
+
+## Screenshot
+![screenshot](./assets/images/screenshot.PNG)
+
+## links
+
+https://github.com/melbrewer323/weather-dashboard
+
+https://melbrewer323.github.io/weather-dashboard/
+
+
--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,18 @@
-function getApi() {
-  // fetch request gets a list of all the repos for the node.js organization
-  var requestUrl = 'https://openweathermap.org/api/one-call-api';
+var APIKey = "1aad1d2ff9ff11a4cfcf3778766ed9c6";
+var button = document.querySelector(".btn");
+var searchInput = document.querySelector("#search-input");
+
+function getApi(){
+ 
+  var requestUrl = 'http://api.openweathermap.org/data/2.5/weather?q=san%20antonio&appid=1aad1d2ff9ff11a4cfcf3778766ed9c6';
 
   fetch(requestUrl)
     .then(function(response) {
       return response.json();
-    }).then(function (data) {
+    })
+    .then(function (data) {
       console.log(data);
     })
 
-}
+};
 getApi()
