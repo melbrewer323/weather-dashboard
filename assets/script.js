a96b6c956bd8fb3856860919c7f9403eaa2b2265
Melissa Brewer <melramirez727@gmail.com>
2021-06-08 22:19:48 -0500

    request url

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,13 @@
-console.log("hi");
+function getApi() {
+  // fetch request gets a list of all the repos for the node.js organization
+  var requestUrl = 'https://openweathermap.org/api/one-call-api';
+
+  fetch(requestUrl)
+    .then(function(response) {
+      return response.json();
+    }).then(function (data) {
+      console.log(data);
+    })
+
+}
+getApi()
